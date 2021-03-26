dc1708f21484cd4ae3ccca7196da17eec9f277ac
mountnside <mountnside@protonmail.com>
2021-03-26 16:50:56 +1100

    format.

--- src/parallel_imp.h
+++ src/parallel_imp.h
@@ -1,24 +1,24 @@
 
 #include "gfx_m64p.h"
 #include "glguts.h"
-#include "gfxstructdefs.h" 
-
-
+#include "gfxstructdefs.h"
+#include <memory>
+#include <vector>
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-extern int32_t vk_rescaling;
-extern bool vk_ssreadbacks;
-extern bool vk_ssdither;
+    extern int32_t vk_rescaling;
+    extern bool vk_ssreadbacks;
+    extern bool vk_ssdither;
 
     void vk_rasterize();
     void vk_process_commands();
-bool vk_init();
-void vk_destroy();
+    bool vk_init();
+    void vk_destroy();
 
 #ifdef __cplusplus
 }
 #endif
-
